f76f25ff2ee900c3180eabab55c04d5aae0fa274
Sagar Biswas <58793583+MrSagarBiswas@users.noreply.github.com>
2021-10-05 06:39:28 +0530

    Variable Created

--- Auto_sms.py
+++ Auto_sms.py
@@ -1,9 +1,10 @@
 from time import sleep
-import pyautogui as p
+import pyautogui as py
 
+sms = input("Write The SMS: ")
+times = input("Number of Times to Sent: ")
 sleep(5)
-
-for i in range(1000):
+for i in range(times):
     sleep(0.1)
-    p.typewrite("Oi", 1)
+    p.typewrite(sms, 0.1)
     p.press("enter")
